0aeb19cfda98bfd6f88ef6421aecd8a9a19bd6d6
PedroLuisVeraBazurto <92538964+PedroLuisVeraBazurto@users.noreply.github.com>
2022-04-23 21:35:19 -0500

    Add files via upload

---
+++ Tareas/Tarea 1/ejercicio#1.js
@@ -0,0 +1,9 @@
+//Crear una función que recuba a N como parametro y genere la tabla de multiplicar por  consola de este parametro
+function Tabla (number){
+let multiplicar = 0;
+for (let k = 0; num <=10; num = num + 1){
+    multiplicar = k* num;
+    console.log(k+ " * " + num+ " = " + multiplicar);
+}
+}
+Tabla(500); 
---
+++ Tareas/Tarea 1/ejercicio#2.js
@@ -0,0 +1,7 @@
+/* Crear un objeto que tenga como parametro, ingredientes, tipo (ensalada, plato fuerte, entrada, etc.)
+y cualquier otro atributo que considere importante para su diseño.*/
+
+let alimento = {
+    Ingredientes : 'Carne', 'condimentos': 'Pepino', 'Tomate': 'Aceite', 'Limón:': 'Cebolla',
+    Tipo : 'Plato fuerte'
+}
