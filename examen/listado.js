4db4f7fbf739b93343f03a0230cf4a26082e80dc
PedroLuisVeraBazurto <92538964+PedroLuisVeraBazurto@users.noreply.github.com>
2022-06-02 20:08:33 -0500

    Add files via upload

---
+++ examen/listado.js
@@ -0,0 +1,39 @@
+class calificacion {
+    constructor(nombre,nota,materia)
+    {
+        this.nombre=nombre;
+        this.nota=nota;
+    }
+    estudiante1 = {
+        nombre:"Pedro Vera",
+        nota:"5",
+        nota2:"8",
+        materia:"Aplicaciones web"
+    }
+    estudiante2 = {
+        nombre: "Luis Bazurto",
+        nota: "7",
+        nota2:"10",
+        materia:"Aplicaciones web"
+    }
+    estudiante3={
+        nombre:"Eber Alarcon",
+        nota:"10",
+        nota2:"10",
+        materia:"Aplicaciones web"
+    }
+}
+let calificacion = nota 
+//console.log(estudiantes)
+/*function operacion(nota,nota2)
+{
+    switch (operador){
+        case "+": 
+        return nota + nota2 ;
+        default: 
+        return 0;
+    }
+}
+function 
+
+ console.log("")*/
