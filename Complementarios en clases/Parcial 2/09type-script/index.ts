f7f4ba97f2eedd4f4e216ab8f1378598b65efe8d
PedroLuisVeraBazurto <pvera9323a@gmai.com>
2022-06-09 16:23:22 -0500

    Update

--- Complementarios en clases/Parcial 2/09type-script/index.ts
+++ Complementarios en clases/Parcial 2/09type-script/index.ts
@@ -1,17 +1,18 @@
-//npm init -y
+/*npm init -y
 //npm i express
 //Instalar TypeScript de manera global
 //npm install -g typescript
 //tsc --init
 //npm i typescript
-import express from 'express'
-
-const express = require ('express');
+//import express from 'express'
+import express, {Request, Response} from 'express'
+//const express = require ('express');
 
 const app = express ();
 const port = 3000;
 
-app.get('/', (req, res) =>{
+
+app.get('/', (req: Request, res: Response) =>{
     res.json({
         msg: 'ok'
     })
@@ -26,4 +27,11 @@ app.listen (port, () =>{
 //Instalar cors   npm i dotenv             npm i mongoose      npm i nodemon-d
 //Instalar dotenv
 //Instalar express-validator
-//Instalar mongoose
+//Instalar mongoose*/
+
+import { config } from "dotenv"
+config()
+
+import {Server} from './server'
+const server = new Server()
+server.listen()
